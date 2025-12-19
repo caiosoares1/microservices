75d9fe5763b6ad5fa13e83dcbf0f72dc476d8cc9
caiosoares1 <batistacaio2244@gmail.com>
2025-12-18 21:42:15 -0300

    parte 2

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/caiosoares1/microservices/order/config"
 	"github.com/caiosoares1/microservices/order/internal/adapters/db"
+	"github.com/caiosoares1/microservices/order/internal/adapters/payment"
 	//"github.com/caiosoares1/microservices/order/internal/adapters/rest"
 	"github.com/caiosoares1/microservices/order/internal/adapters/grpc"
 	"github.com/caiosoares1/microservices/order/internal/application/core/api"
@@ -15,7 +16,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to connect to database. Error: %v", err)
 	}
-	application := api.NewApplication(dbAdapter)
+
+	paymentAdapter, err := payment.NewAdapter(config.GetPaymentServiceUrl())
+	if err != nil {
+		log.Fatalf("Failed to initialize payment stub. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter, paymentAdapter)
 	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
 	grpcAdapter.Run()
 }
--- order/go.mod
+++ order/go.mod
@@ -4,6 +4,7 @@ go 1.25.3
 
 require (
 	github.com/caiosoares1/microservices-proto/golang/order v0.0.1
+	github.com/caiosoares1/microservices-proto/golang/payment v0.0.0-00010101000000-000000000000
 	gorm.io/gorm v1.31.1
 )
 
@@ -23,3 +24,5 @@ require (
 	google.golang.org/grpc v1.77.0
 	gorm.io/driver/mysql v1.6.0
 )
+
+replace github.com/caiosoares1/microservices-proto/golang/payment => ../../../microservices-proto/golang/payment
