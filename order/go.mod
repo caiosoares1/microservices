ebef2d6e0559bf96a91ab5863d4c9f7a37f849ab
caiosoares1 <batistacaio2244@gmail.com>
2026-01-20 20:58:41 -0300

    parte 4

--- order/go.mod
+++ order/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,11 +2,16 @@ package payment_adapter
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/caiosoares1/microservices-proto/golang/payment"
 	"github.com/caiosoares1/microservices/order/internal/application/core/domain"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Adapter struct {
@@ -15,6 +20,12 @@ type Adapter struct {
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
+	opts = append(opts,
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			grpc_retry.WithMax(5),
+			grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+		)))
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
 	if err != nil {
@@ -25,10 +36,21 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	_, err := a.payment.Create(ctx, &payment.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
 	})
-	return err
+
+	if err != nil {
+		if status.Code(err) == codes.DeadlineExceeded {
+			log.Printf("TIMEOUT: A chamada ao serviço Payment excedeu o limite de 2 segundos para o pedido %d", order.ID)
+		}
+		return err
+	}
+
+	return nil
 }
